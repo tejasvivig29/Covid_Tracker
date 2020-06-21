f54e79a902681ed7a9f6d4d398ee6326ee130214
Tejasvi Vig <tejasvivig09@gmail.com>
2020-06-20 23:27:47 -0300

    API data fetch and displaey in cards

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CountryPicker extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>
+                    CountryPicker
+                </h1>
+            </div>
+        )
+    }
+
+    }
+    
+
+export default CountryPicker;

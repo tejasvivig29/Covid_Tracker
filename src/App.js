928632485eb0b763deb76cd1cdf958e36551de79
Tejasvi Vig <tejasvivig09@gmail.com>
2020-07-02 17:00:26 -0300

    Tracking feature implemented along with styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React from 'react';
 
-import { Cards , Chart , CountryPicker } from './components';
+import Cards from './components/Cards/Cards';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import Chart from './components/Chart/Chart';
+import { fetchData } from './api/';
 import styles from './App.module.css';
-import { fetchData } from './api';
 
-class App extends React.Component{
+import image from './images/image.png';
 
-    state = {
-        data: {},
-    }
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
 
-    async componentDidMount(){
-        const fetchedData = await fetchData();
-        this.setState({data : fetchedData});
-    }
+  async componentDidMount() {
+    const data = await fetchData();
 
-    render(){
-        const { data } = this.state;
-        return(<div className={styles.container}>
-            <Cards data={data}/>
-            <CountryPicker />
-            <Chart />
-        </div>)
-    }
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
 }
 
 export default App;
